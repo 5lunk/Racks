fb462946d4423627cbe410fab82298f9ff1afa47
dzmartli <desaccorde@gmail.com>
2024-04-10 15:13:41 +0400

    user cleanup

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,6 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
  *
- * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
