d4466b5ec7686380dce956285f0cedf2c0ff8e66
dzmartli <desaccorde@gmail.com>
2024-05-22 12:47:35 +0400

    message component improvement

--- frontend/src/components/TheMessage.vue
+++ frontend/src/components/TheMessage.vue
@@ -1,13 +1,14 @@
 <template>
-  <template v-if="message.text.length && message.success">
-    <text>
-      {{ message.text }}
-    </text>
-  </template>
-  <template v-else-if="message.text.length && !message.success">
-    <text class="text-red-500">
-      {{ message.text }}
-    </text>
+  <template v-if="message.text.length">
+  <text :class="messageStyle">
+    {{ message.text }}
+    <sup>
+      <button class="text-xm text-slate-500 sups"
+              v-on:click="acknowledge">
+        &#215;
+      </button>
+    </sup>
+  </text>
   </template>
   <template v-else>
     <text></text>
@@ -23,8 +24,21 @@ export default {
       success: Boolean,
     },
   },
+  computed: {
+    messageStyle() {
+      if (this.message.text.length && !this.message.success) {
+        return 'text-red-500';
+      }
+      return ''
+    }
+  },
   mounted() {
     this.$store.commit('setMessageDefaults');
+  },
+  methods: {
+    acknowledge() {
+      this.$store.commit('setMessageDefaults');
+    }
   }
 };
 </script>
