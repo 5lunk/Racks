180f776ca3048e6a8e6ba268074ffb4bf62b9965
dzmartli <desaccorde@gmail.com>
2024-05-23 20:58:04 +0400

    del blank line

--- app/Enums/StatusCodeEnum.php
+++ app/Enums/StatusCodeEnum.php
@@ -11,7 +11,6 @@ enum StatusCodeEnum: int
     case ACCEPTED = 202;
     case NO_CONTENT = 204;
     case BAD_REQUEST = 400;
-
     case FORBIDDEN = 403;
     case NOT_FOUND = 404;
     case INTERNAL_SERVER_ERROR = 500;
